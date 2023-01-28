ceb295770483fc24dd8e20b43154b56bf5409705
Joanadgomes <joanadiogog@gmail.com>
2023-01-28 19:01:35 +0000

    eliminate the aceleration

--- scripts/main.js
+++ scripts/main.js
@@ -19,18 +19,18 @@ document.addEventListener('keydown', (e) => {
   switch (e.code) {
     case 'ArrowUp':
       player.movementX = 0;
-      player.movementY -= 2;
+      player.movementY = -2;
       break;
     case 'ArrowDown':
       player.movementX = 0;
-      player.movementY += 2;
+      player.movementY = 2;
       break;
     case 'ArrowLeft':
-      player.movementX -= 2;
+      player.movementX = -2;
       player.movementY = 0;
       break;
     case 'ArrowRight':
-      player.movementX += 2;
+      player.movementX = 2;
       player.movementY = 0;
       break;
     case 'ArrowUp' + 'ArrowUp':
